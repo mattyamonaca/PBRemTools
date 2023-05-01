87c9ba24176027e667540f70b49621d70b3bda69
udon-universe <128375799+udon-universe@users.noreply.github.com>
2023-05-02 01:34:34 +0900

    add extension name

--- install.py
+++ install.py
@@ -13,7 +13,7 @@ packages = [
 
 for package in packages:
     if not launch.is_installed(package):
-        launch.run_pip(f'install {package}', desc=package)
+        launch.run_pip(f'install {package}', desc=f'{package} for PBRemTools')
 
 if not launch.is_installed("segment_anything"):
     launch.run_pip("install git+https://github.com/facebookresearch/segment-anything.git", desc="segment_anything")
